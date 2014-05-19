5b00d969ef10a422860134b4ec7773aa42632558
nithinvadekkepat <nithin@castalialabs.com>
2014-05-19 09:17:58 +0530

    updated the main program, included logo images

--- Gige/getCamImage.m
+++ Gige/getCamImage.m
@@ -1,7 +1,23 @@
 function getCamImage()
+close all;clear all;
+imaqreset;
+date = datestr(clock,'dd-mm-yy');
+ret = exist(date,'dir');
+if ~ret
+    mkdir(date);
+end
+currentPath = pwd;
+writePath = strcat(currentPath,'\',date);
+logo = imread('logo_drishtiman.png');
 mainFig = figure('position',[100 60 1200 650 ],'MenuBar','none','name',...
     'Gige Camera Acquisition','NumberTitle','off','Resize','off');
 logoAxes = axes('Parent',mainFig,'Units', 'normalized','Position',[0.02 0.88 0.1 0.1],'Visible','on');
+set(mainFig,'CurrentAxes',logoAxes)
+imshow(logo)
+drawnow
+axis image
+axis tight
+axis off
 uicontrol('Style','pushbutton','Parent', mainFig,'Units','normalized',...
     'HandleVisibility','callback', ...
     'Position',[0.05 0.08 0.1 0.05],...
@@ -17,11 +33,16 @@ uicontrol('Style','pushbutton','Parent', mainFig,'Units','normalized',...
     'Position',[0.5 0.08 0.1 0.05],...
     'String','Start Process',...
     'Callback', @processButtonCallBack);
+uicontrol('Style','pushbutton','Parent', mainFig,'Units','normalized',...
+    'HandleVisibility','callback', ...
+    'Position',[0.8 0.08 0.1 0.05],...
+    'String','Exit Process',...
+    'Callback', @exitButtonCallback);
 camAxes =  axes('Parent',mainFig,'Units', 'normalized','Position',[0.02 0.2 0.4 0.6],'Visible','on');
 
-acqAxes =  axes('Parent',mainFig,'Units', 'normalized','Position',[0.5 0.2 0.4 0.6],'Visible','on');
+acqAxes =  axes('Parent',mainFig,'Units', 'normalized','Position',[0.5 0.2 0.4 0.6],'Visible','off');
 set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
-vid = videoinput('winvideo',1);
+vid = videoinput('winvideo',2);
 vid.FramesPerTrigger = 1;
 vid.ReturnedColorspace = 'rgb';
 triggerconfig(vid, 'manual');
@@ -31,13 +52,14 @@ imHeight = vidRes(2);
 nBands = get(vid, 'NumberOfBands');
 backBuffer = 5;
 background = zeros(imHeight,imWidth);
-blobObj = vision.BlobAnalysis('AreaOutputPort','True','MaximumBlobArea',1000);
-
+blobObj = vision.BlobAnalysis('AreaOutputPort',1,'MinimumBlobArea',1000);
+set(mainFig,'CurrentAxes',camAxes)
+hImage = image(zeros(imHeight, imWidth, nBands));
+axis tight
+axis off
 
 
     function startCamButtonCallBack(src, eventdata)
-        set(mainFig,'CurrentAxes',camAxes)
-        hImage = image(zeros(imHeight, imWidth, nBands));
         preview(vid,hImage)
         axis image
         axis tight
@@ -48,17 +70,35 @@ blobObj = vision.BlobAnalysis('AreaOutputPort','True','MaximumBlobArea',1000);
         for i = 1:backBuffer
             framegrabber(:,:,i) = rgb2gray(getsnapshot(vid));
         end
-        background = mean(framegrabber,3);       
+        background = mean(framegrabber,3);
     end
     function processButtonCallBack(src,eventdata)
-       setappdata(hImage,'UpdatePreviewWindowFcn',@update_get_object);
+        setappdata(hImage,'UpdatePreviewWindowFcn',@update_get_object);
     end
     function update_get_object(obj,event,hImage)
         currentFrame = event.Data;
+        background = double(background);
+        currentFrame = double(rgb2gray(currentFrame));
         foreground = abs(background - currentFrame);
-        binaryBackground = im2bw(foreground);
+        binaryBackground = (foreground > 15);
+        set(mainFig,'CurrentAxes',acqAxes)
+        imshow(binaryBackground);
+        drawnow
         area = step(blobObj,binaryBackground);
-        area
-        
+        if numel(area) > 0
+            if numel(area) > 1 || area(1) > 50000
+                set(mainFig,'CurrentAxes',acqAxes)
+                text(256,256,'remove hand','FontSize',28,'Color',[1 0 0])
+                drawnow
+            else
+                timestamp = datestr(clock,'HH_MM_SS');
+                imwrite(uint8(currentFrame),fullfile(writePath,strcat(timestamp,'.jpg')));
+            end
+        end
     end
+        
+        function  exitButtonCallback(src, eventdata)
+            stoppreview(vid)
+            close all;
+        end
 end
